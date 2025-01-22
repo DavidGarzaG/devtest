7ac14166aeae959bd333d148254df692b66d361d
DavidGarzaG <dagago97@gmail.com>
2025-01-22 15:41:33 -0600

    changed relationship between entities

--- devtest/main.py
+++ devtest/main.py
@@ -15,7 +15,8 @@ cursor.execute(command1)
 # We could also use the type of building and have different models according to the type of building.
 # Comment: Could divide adress into several subsets, but that is beyond the point.
 command2 = """ CREATE TABLE IF NOT EXISTS
-buildings(building_id INTEGER PRIMARY KEY, building_type TINYTEXT, address TEXT) 
+buildings(building_id INTEGER PRIMARY KEY, building_type TINYTEXT, address TEXT,
+FORIEGN KEY(client_id) REFERENCES clients(client_id)) 
 """
 cursor.execute(command2)
 
@@ -23,7 +24,7 @@ cursor.execute(command2)
 # This table (without the foreign keys) would be sufficient to store the data that we need for one case
 command3 = """ CREATE TABLE IF NOT EXISTS
 elevator(elevator_id INTEGER PRIMARY KEY, timestamp DATETIME, resting_floor INTEGER, floor_demanded INTEGER, out_or_in_demand BOOLEAN, weight FLOAT,
-FORIEGN KEY(client_id) REFERENCES clients(client_id), FORIEGN KEY(building_id) REFERENCES buildings(building_id)
+FORIEGN KEY(building_id) REFERENCES buildings(building_id)
 """
 
 cursor.execute(command3)
